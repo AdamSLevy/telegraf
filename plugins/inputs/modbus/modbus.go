7a9f6dde7c4a7452f10d413ddcbacc09980226de
Adam S Levy <theadamlevy@gmail.com>
2017-11-19 16:41:53 -0900

    Cleaned up old code

--- plugins/inputs/modbus/modbus.go
+++ plugins/inputs/modbus/modbus.go
@@ -1,110 +1,48 @@
 package modbus
 
 import (
-        "fmt"
-        "reflect"
-        //"time"
-        //"log"
-        //"os"
+	"fmt"
+	//"time"
+	//"log"
+	//"os"
 
-        //"github.com/goburrow/modbus"
+	"github.com/AdamSLevy/modbus"
+
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
 type query struct {
-        Function string  `toml:"function"`
-        DataSize *string `toml:"data_size"`
-        DataType string  `toml:"data_type"`
+	Function string
 
-        Register *byte   `toml:"register"`
-        Address  *byte   `toml:"address"`
+	Address  uint16
+	Quantity uint16
 
-        Bit *byte `toml:"bit"`
-
-        Quantity *uint16 `toml:"quantity"`
-
-        Interval *internal.Duration `toml:"interval"`
-}
-
-func print(p interface{}) {
-    v := reflect.ValueOf(p)
-    if reflect.Ptr == v.Kind() {
-        if !v.IsNil() {
-            fmt.Println(*v.Pointer())
-        }
-    if nil == v {
-        fmt.Println(v)
-    } else {
-        fmt.Println(*v)
-    }
-}
-
-func (qry *query) print() {
-    fmt.Println("Query")
-    fmt.Println("Function:", qry.Function)
-    fmt.Println("DataSize:"); print_ptr(qry.DataSize)
-    fmt.Println("DataType:", qry.DataType)
-    fmt.Println("Register:", *qry.Register)
-    fmt.Println("Address: ", *qry.Address)
-    fmt.Println("Bit:     ", *qry.Bit)
-    fmt.Println("Quantity:", *qry.Quantity)
-    fmt.Println("Interval:", *qry.Interval)
-    fmt.Println()
+	Interval internal.Duration
 }
 
 type slave struct {
-        Id      byte    `toml: id`
-        Queries []query `toml:"query"`
+	Id      byte    `toml: id`
+	Queries []query `toml:"query"`
 
-        Interval *internal.Duration `toml:"default_interval"`
-}
-
-func (slv *slave) print() {
-    fmt.Println("Slaves:")
-    fmt.Println("Id:      ", slv.Id)
-    fmt.Println("Interval:", slv.Interval)
-    for i, qry := range slv.Queries {
-        fmt.Print(i, " ")
-        qry.print()
-    }
-    fmt.Println()
+	DefaultInterval internal.Duration
 }
 
 type Modbus struct {
-        Mode        string `toml:"mode"`
+	Mode    string
+	Host    string
+	Port    uint16
+	Baud    uint
+	Timeout internal.Duration
 
-        // TCP
-        Host    *string `toml:"host"`
-        Port    *uint16 `toml:"port"`
+	DefaultInterval internal.Duration
 
-        // RTU || ASCII
-        SerialPort *string `toml:"serial_port"`
-        Baud       uint32  `toml:"baud"`
-
-        Interval *internal.Duration `toml:"default_interval"`
-
-        Slaves     []slave `toml:"slave"`
-}
-
-func (mb *Modbus) print() {
-    fmt.Println("Modbus:")
-    fmt.Println("Mode:      ", mb.Mode)
-    fmt.Println("Host:      ", mb.Host)
-    fmt.Println("Port:      ", mb.Port)
-    fmt.Println("SerialPort:", mb.SerialPort)
-    fmt.Println("Baud:      ", mb.Baud)
-    fmt.Println("Interval:  ", mb.Interval)
-    for i, slv := range mb.Slaves {
-        fmt.Print(i, " ")
-        slv.print()
-    }
-    fmt.Println()
+	Slaves []slave `toml:"slave"`
 }
 
 func (mb *Modbus) SampleConfig() string {
-        return `
+	return `
   ## Connection Config
   ## Only use one of the following configs per [[ output.Modbus ]] section
 
@@ -114,13 +52,13 @@ func (mb *Modbus) SampleConfig() string {
   port = 502               # Defaults to 502 if omitted
 
   ## RTU Config
-  # mode    = "RTU"                # Required
-  # serial_port = "/dev/ttyUSB0"   # Required
-  # baud = 9600                    # Defaults to 9600
+  # mode = "RTU"            # Required
+  # host = "/dev/ttyUSB0"   # Required
+  # baud = 9600             # Defaults to 9600
 
   ## ASCII Config
-  # mode    = "ASCII"              # Required
-  # serial_port = "/dev/ttyUSB0"   # Required
+  # mode = "ASCII"              # Required
+  # host = "/dev/ttyUSB0"   # Required
   # baud = 9600                    # Defaults to 9600
 
   ## The default query interval for all slaves
@@ -135,31 +73,54 @@ func (mb *Modbus) SampleConfig() string {
 
     [[inputs.modbus.slave.query]]
       function = "read_coil"
-      register = 1
+      address = 1
 
 
     [[inputs.modbus.slave.query]]
       function = "read_coils"
-      register = 1
+      address = 1
 `
 }
 
 func (mb *Modbus) Gather(acc telegraf.Accumulator) error {
-        return nil
+	return nil
 }
 
 func (mb *Modbus) Start(acc telegraf.Accumulator) error {
-    mb.print()
-    return nil
+	mode, ok := modbus.ModeByName[mb.Mode]
+	if !ok {
+		return fmt.Errorf("Unknown Mode: %v\n", mb.Mode)
+	}
+	switch mode {
+	case modbus.ModeTCP:
+		if 0 == mb.Port {
+			mb.Port = 502
+		}
+		mb.Host = mb.Host + fmt.Sprintf(":%v", mb.Port)
+	case modbus.ModeASCII:
+		fallthrough
+	case modbus.ModeRTU:
+		if 0 == mb.Baud {
+			mb.Baud = 9200
+		}
+	}
+
+	cs := modbus.ConnectionSettings{
+		Mode: mode,
+		Host: mb.Host,
+		Baud: mb.Baud,
+	}
+	fmt.Println("Connection Settings:", cs)
+	return nil
 }
 
 func (mb *Modbus) Stop() {
-    return
+	return
 }
 
 func (mb *Modbus) Description() string {
 	return "Read multiple data points from multiple slaves over Modbus ASCII, RTU, " +
-                "or TCP."
+		"or TCP."
 }
 
 func init() {
